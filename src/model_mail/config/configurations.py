07cc6664b4d4ed51cef333a6c2ec5e9defbcd3f1
Cam <cammclain@skiff.com>
2024-07-28 09:54:18 -0500

    chore: Add MailingConfigs class to handle mailing configurations
    This commit adds the `MailingConfigs` class to the `configurations.py` file. This class is responsible for handling the mailing configurations, including the SMTP server, port, user, password, sender email, and receiver email. The class uses the `python-dotenv` package to load the configurations from environment variables.

---
+++ src/model_mail/config/configurations.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from rich.console import Console
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+class MailingConfigs:
+    def __init__(self, console: Console) -> None:
+        self.console: Console = console
+        self.smtp_server: str = os.getenv("SYSTEM_SMTP_SERVER")
+        self.smtp_port: int = int(os.getenv("SYSTEM_SMTP_PORT"))
+        self.smtp_user: str = os.getenv("SYSTEM_SMTP_USER")
+        self.smtp_password: str = os.getenv("SYSTEM_SMTP_PASSWORD")
+        self.sender_email: str = os.getenv("SYSTEM_SENDER_EMAIL")
+        self.receiver_email: str = os.getenv("SYSTEM_RECEIVER_EMAIL")
