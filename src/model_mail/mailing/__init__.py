5ccd3a49c9e0a04fc6079d3b215adeefdf7ee049
Cam <cammclain@skiff.com>
2024-08-09 01:48:33 -0500

    chore: Add Mailing module init file
    This commit adds the initial version of the Mailing module's init file. The goal of this module is to provide a way to parse incoming emails from users and send emails to the correct users in response.

---
+++ src/model_mail/mailing/__init__.py
@@ -0,0 +1,12 @@
+# Mailing module init file
+'''
+The goal of this module is to provide a way to both 
+- parse incoming emails from users
+- send emails to the correct users to reply to the incoming emails
+
+For this, we will need to implement a way to parse the incoming emails and extract the necessary information to send the correct email to the correct user.
+
+
+
+
+'''
